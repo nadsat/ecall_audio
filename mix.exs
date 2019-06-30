6899c1d23c31b4c7d20e3cb96b9f639e9262a590
nadsat <antonio.martinez.c@protonmail.com>
2019-06-30 12:49:16 -0400

    ADD: package info

--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule Ecall.Audio.MixProject do
   use Mix.Project
-  @version "0.1.0"
+  @version "0.0.1"
   def project do
     [
       app: :ecall_audio,
       version: @version,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      docs: [extras: ["README.md"], main: "readme"],
       deps: deps()
     ]
   end
@@ -21,7 +24,19 @@ defmodule Ecall.Audio.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:circuits_uart, "~> 1.2"}
+      {:circuits_uart, "~> 1.2"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Make phone calls using AT commands."
+  end
+
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/nadsat/ecall_control"}
     ]
   end
 end
